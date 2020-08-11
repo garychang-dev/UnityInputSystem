ad29261db77abf759bf6a13b429f0203f4f8c724
Gary Chang <gary.chang.dev@gmail.com>
2020-08-11 02:18:11 -0400

    Implemented a sample with two input action types

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    public float jumpForce;
+
+    private Rigidbody m_Rigidbody;
+    private Vector3 m_Direction;
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 offset = m_Direction * speed * Time.deltaTime;
+        Vector3 nextPosition = transform.position + offset;
+        m_Rigidbody.MovePosition(nextPosition);
+    }
+
+    public void Move(Vector3 direction)
+    {
+        m_Direction = direction;
+    }
+
+    public void Jump()
+    {
+        m_Rigidbody.AddForce(Vector3.up * jumpForce);
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Controls m_Controls;
+
+    private PlayerController m_PlayerController;
+    void Awake()
+    {
+        m_Controls = new Controls();
+        m_PlayerController = GetComponent<PlayerController>();
+
+        // Action Type: Action Asset
+        m_Controls.Player.Jump.performed += OnJump;
+    }
+
+    void OnEnable()
+    {
+        m_Controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        m_Controls.Disable();
+    }
+    
+    void Update()
+    {
+        // Action Type: Value
+        Vector2 inputDirection = m_Controls.Player.Move.ReadValue<Vector2>();
+        Vector3 direction = new Vector3(inputDirection.x, 0f, inputDirection.y);
+        direction = Vector3.ClampMagnitude(direction, 1f);
+
+        m_PlayerController.Move(direction);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        m_PlayerController.Jump();
+    }
+}
